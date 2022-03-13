9990920471a64d18d702aaf11caa59ea37ba96be
Capibarik <88647226+Capibarik@users.noreply.github.com>
2022-03-13 22:05:16 +0800

    Add files via upload

---
+++ Задача_с_массивом/Задача_с_массивом/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задача_с_массивом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] mas = new int[8];
+            int[] new_mas = new int[8];
+            for (uint i = 0; i <= 7; i++)
+            {
+                mas[i] = rnd.Next(1, 20);
+                Console.Write(mas[i] + ", ");
+            }
+            Console.WriteLine("\n+6 ко всем элементам массива");
+            for (uint i = 0; i <= 7; i++)
+            {
+                new_mas[i] = mas[i] + 6;
+                Console.Write(new_mas[i] + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
