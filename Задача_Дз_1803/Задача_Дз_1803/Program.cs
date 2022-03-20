e667aea988ed3f5afb6092babfebcabdfbce3f6e
Capibarik <88647226+Capibarik@users.noreply.github.com>
2022-03-20 19:46:15 +0800

    Add files via upload

---
+++ Задача_Дз_1803/Задача_Дз_1803/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задача_Дз_1803
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            double S = 0;
+            do
+            {
+                Console.Write("Введите положительное n:");
+                n = Convert.ToDouble(Console.ReadLine());
+            }
+            while (n <= 0);
+            for (int i = 0; i <= n; i++)
+            {
+                S += Math.Pow(n + i, 2);
+            }
+            Console.WriteLine($"Ответ {S}");
+            Console.ReadKey();
+        }
+    }
+}
